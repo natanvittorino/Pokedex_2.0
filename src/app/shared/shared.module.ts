4bbfe08a3833504b188c77365abd4eca6f5c0317
natanvittorino <natanvittorino2020@gmail.com>
2022-01-18 11:05:15 -0300

    progress-bar component has been created inside shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProgressBarComponent
+  ],
   imports: [
     CommonModule
   ]
